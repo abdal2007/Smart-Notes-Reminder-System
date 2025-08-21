de4eb46cdcef3969a46eed3479e2a95a35a686a4
AHMAD ABDAL <abdalmaster4@gmail.com>
2025-08-21 12:24:39 +0500

    Create forms.py

---
+++ AI_smart_notes/smartnotes/notes/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SimpleSignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def clean_password2(self):
+        pw1 = self.cleaned_data.get("password1")
+        pw2 = self.cleaned_data.get("password2")
+        if pw1 and pw2 and pw1 != pw2:
+            raise forms.ValidationError("Passwords do not match!")
+        return pw2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
